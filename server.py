9568378ea1dcf76bdba27e18eb2b8566083d9fd1
Alexander Borges <borges2721@gmail.com>
2023-07-21 01:06:48 -0400

    updated assigment for CRUD

--- server.py
+++ server.py
@@ -22,6 +22,32 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {
+        "id":id
+    }
+    return render_template("edit_user.html",user=User.get_one(data))
+
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/delete/<int:id>')
+def destroy(id):
+    data = {
+        'id': id
+    }
+    User.delete(data)
+    return redirect('/users')
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data = {
+        'id': id
+    }
+    return render_template("show_user.html", user=User.get_one(data))
 
 
 if __name__ == "__main__":
